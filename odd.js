edba957341b339e700035f63c6481b673c1c59e7
Jackson Stark <jacksonstark77@hotmail.com>
2019-07-05 16:31:30 +0000

    repo = light-js-fun

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = (n) => {
+  if (n % 2 !== 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+
